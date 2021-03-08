cab930983d31725cf71ba95bdd4306f1d07b748c
Dan Song <dansong0729@gmail.com>
2021-03-08 18:09:53 -0500

    Fix bug in relative entropy derivative
    Integrands in find_d_dx_D_PYgivenX_PY.m in both Amplitude Constrained
    and Power Constrained folders were missing factor of ln(2) on the +1
    term, as well as containing an extraneous factor of 1/2 overall. Due
    to odd symmetry in the integral, this does not affect results of DAB.
    However, it may cause issues if used to compute the Csiszar bound for
    other applications.

--- Amplitude Constrained AWGN DAB/find_d_dx_D_PYgivenX_PY.m
+++ Amplitude Constrained AWGN DAB/find_d_dx_D_PYgivenX_PY.m
@@ -6,7 +6,7 @@ g = @(y) PY(y);
 pad = 5*sqrt(N);
 ymin = min(xSupport);
 ymax = max(xSupport);
-arg = @(y) ((y-x)/(2*N)).*f(y).*(log2(f(y)./g(y))+1);
+arg = @(y) ((y-x)/N).*f(y).*(log2(f(y)./g(y))+1/log(2));
 d_dx_D_PYgivenX_PY = integral(arg, ymin - pad, ymax + pad);
 % test 
 % d_dx_D_PYgivenX_PY = integral(arg, 0-pad,1+pad);
